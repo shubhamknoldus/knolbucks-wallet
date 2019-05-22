6eab5783d1034f3bb83e7f31129076445f6c6e66
Sidharth <sidharth@knoldus.com>
2019-05-22 11:22:03 +0530

    Removed sample unrequired code for SQL connections

--- src/test/java/com/knoldus/knolbucks/CouponsApplicationTests.java
+++ src/test/java/com/knoldus/knolbucks/CouponsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.knoldus.knolbucks;
 
-import org.davidmoten.rx.jdbc.Database;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +11,6 @@ public class CouponsApplicationTests {
 
     @Test
     public void contextLoads() {
-        /*TODO: NJ - This is just a sample code for database connection created for testing purpose so will be removed*/
-        Database db = Database.from("jdbc:mysql://localhost:3306/knolbucks?user=root&password=root", 10);
-        db.select("select LastName from Persons")
-               .getAs(String.class).blockingForEach(System.out::println);
-
     }
 
 }

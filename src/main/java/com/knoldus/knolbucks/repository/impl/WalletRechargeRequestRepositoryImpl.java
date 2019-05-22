ffd13e37fd508db55ad6397c800ca04100678e43
jainnancy <nancy.jain@knoldus.com>
2019-05-22 18:16:22 +0530

    Implemented findAll method for WalletRechargeRequestRepository

--- src/main/java/com/knoldus/knolbucks/repository/impl/WalletRechargeRequestRepositoryImpl.java
+++ src/main/java/com/knoldus/knolbucks/repository/impl/WalletRechargeRequestRepositoryImpl.java
@@ -3,25 +3,32 @@ package com.knoldus.knolbucks.repository.impl;
 import com.knoldus.knolbucks.model.WalletRechargeRequest;
 import com.knoldus.knolbucks.repository.WalletRechargeRequestRepository;
 import com.knoldus.knolbucks.repository.utils.DBObject;
+import reactor.core.publisher.Flux;
 
 import java.sql.Timestamp;
 import java.util.Date;
 
 public class WalletRechargeRequestRepositoryImpl implements WalletRechargeRequestRepository {
 
+    final String tableName = "wallet_recharge_request";
+
     public Boolean save(WalletRechargeRequest walletRechargeRequest) {
 
-        String insertQuery = "insert into wallet_recharge_request values(?, ?, ?, ?, ?, ?, ?, ?)";
+        String insertQuery = "insert into " + tableName + " values(?, ?, ?, ?, ?, ?, ?, ?)";
         return DBObject.getDBObject().update(insertQuery)
                 .parameters(walletRechargeRequest.id(), walletRechargeRequest.walletID(), walletRechargeRequest.requesterID(), walletRechargeRequest.issuerID(), walletRechargeRequest.requestedOn(),
                         walletRechargeRequest.approvedOn(), walletRechargeRequest.quantity(), walletRechargeRequest.status()).counts().blockingFirst() > 0;
+    }
+
+    Flux<WalletRechargeRequest> findAll() {
+        return Flux.fromIterable(DBObject.getDBObject().select("select * from " + tableName ).autoMap(WalletRechargeRequest.class).blockingIterable());
     }
 
     public static void main(String args[]) {
         WalletRechargeRequest w = new WalletRechargeRequest() {
             @Override
             public String id() {
-                return "id11";
+                return "id111";
             }
 
             @Override
@@ -59,7 +66,8 @@ public class WalletRechargeRequestRepositoryImpl implements WalletRechargeReques
                 return "Pending";
             }
         };
-        System.out.println(new WalletRechargeRequestRepositoryImpl().save(w));
+//        System.out.println(new WalletRechargeRequestRepositoryImpl().save(w));
+        new WalletRechargeRequestRepositoryImpl().findAll().toStream().forEach(System.out::println);
     }
 
 }
